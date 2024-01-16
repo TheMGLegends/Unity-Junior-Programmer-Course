22d7763b940a895be113f5ef8695406ae5392ad1
TheMGLegends <matejovo34@gmail.com>
2024-01-16 11:11:21 +0000

    Created new Project for Challenge 2

---
+++ Play Fetch Simulator Prototype/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    [SerializeField] private float spawnDelay = 0.5f;
+
+    private float spawnTimer = 0.0f;
+    private bool bCanSpawn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // INFO: Prevents dogs from being spawned instantly
+        if (!bCanSpawn)
+            spawnTimer += Time.deltaTime;
+
+        if (spawnTimer > spawnDelay )
+        {
+            spawnTimer = 0.0f;
+            bCanSpawn = true;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && bCanSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            bCanSpawn = false;
+        }
+    }
+}
---
+++ Play Fetch Simulator Prototype/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnRandomBall), startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // INFO: Choose a random ball from the array
+        int ballIndex = Random.Range(0, ballPrefabs.Count());
+
+        // INFO: Adjust Spawn Interval
+        spawnInterval = Random.Range(3.0f, 5.0f);
+
+        CancelInvoke();
+        InvokeRepeating(nameof(SpawnRandomBall), spawnInterval, spawnInterval);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
